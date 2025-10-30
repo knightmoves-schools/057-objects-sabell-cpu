8eb9fd9b1a92f483ec24c3be219a4af93e757ce8
sabell-cpu <sabell@anamosa.k12.ia.us>
2025-10-30 18:02:49 +0000

    objects

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let school = {
-    name: "Citlali",
-    "average class size": 3,
-    mascot: "dragon",
+    name: "Bingooooo",
+    "average class size": 4,
+    mascot: "Caleb",
 };
 
-let string = "${school.name} has an average class size of ${school[average class size]} an their mascot is ${school.mascot}";
-document.getElementById("result").innerHTML = string;
+let string = `${school.name} has an average class size of ${school["average class size"]} and their mascot is a ${school.mascot}`;
+document.getElementById('result').innerHTML = string;
 
 //should assign the innerHTML of the HTML element with
 // the id result to the  string "${name} has an 
