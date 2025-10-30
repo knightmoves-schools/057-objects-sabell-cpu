ff9944a50239e28899cb9817f2a8f6764a9f44ca
sabell-cpu <sabell@anamosa.k12.ia.us>
2025-10-30 13:42:54 +0000

    YAY!!! First lesson I did since 37 without help or AI!!!!!

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+let school = {
+    name: "Citlali",
+    "average class size": 3,
+    mascot: "dragon",
+};
 
+document.getElementById("result").innerHTML = '${name} has an average class size of ${average class size} an their mascot is ${mascot}';
+
+//should assign the innerHTML of the HTML element with
+// the id result to the  string "${name} has an 
+// average class size of ${average class size} and 
+// their mascot is a ${mascot}"
