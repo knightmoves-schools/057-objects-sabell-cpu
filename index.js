13b1721a36a008ded42485ed830d15a66c89a227
sabell-cpu <sabell@anamosa.k12.ia.us>
2025-10-30 17:56:33 +0000

    I did it yay!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let school = {
     mascot: "dragon",
 };
 
-document.getElementById("result").innerHTML = "${name} has an average class size of ${average class size} an their mascot is ${mascot}";
+let string = "${name} has an average class size of ${average class size} an their mascot is ${mascot}";
+document.getElementById("result").innerHTML = string;
 
 //should assign the innerHTML of the HTML element with
 // the id result to the  string "${name} has an 
